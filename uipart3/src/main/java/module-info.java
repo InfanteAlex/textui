934dd611f5425792c318cea5551df47ee0c38e5f
InfanteAlex <132919886+InfanteAlex@users.noreply.github.com>
2023-07-21 12:40:27 -0400

    Delete uipart3/src directory

--- uipart3/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.uipart3 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.mycompany.uipart3 to javafx.fxml;
-    exports com.mycompany.uipart3;
-}
